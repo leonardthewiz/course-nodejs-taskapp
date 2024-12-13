55ea35267baf632650379ac9bc3952df2552f5b3
leonardthewiz <travis.j.s.taylor@gmail.com>
2024-12-13 15:54:44 -0500

    changes routes

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,10 +2,26 @@ require('../src/db/mongoose')
 const User = require('../src/models/user')
 
 User.findByIdAndUpdate('674227304c4dc9b2d7d27a91',{age:1}).then((user)=>{
+    console.log("user:")
     console.log(user)
     return User.countDocuments({age:1})
 }).then((result)=>{
+    console.log("result:")
     console.log(result)
 }).catch((e)=>{
+    console.log("error: ")
     console.log(e)
+})
+
+const updateAgeAndCount = async (id,age)=>{
+    const user = await User.findByIdAndUpdate(id,{age})
+    const count = await User.countDocuments({age})
+    console.log("count is ")
+    console.log(count)
+    return count
+}
+
+updateAgeAndCount('67421d856b3e2424d8d3a476').then((count)=>{
+    console.log("count of users same age")
+    console.log(count)
 })
